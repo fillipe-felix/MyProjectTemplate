8f44bb087c586dfa2a0997146b5f2149a3fd12f8
fillipe.felix <felipesoares_1993@hotmail.com>
2025-09-18 20:29:21 -0300

    Add `DbSet<Example>` to `AppDbContext`

--- src/MyProjectTemplate.Infra/Data/AppDbContext.cs
+++ src/MyProjectTemplate.Infra/Data/AppDbContext.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+using MyProjectTemplate.Domain.Entities;
+
 namespace MyProjectTemplate.Infra.Data;
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+    public DbSet<Example> Examples { get; set; }
 }
